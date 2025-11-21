f91a21975a04c8f1b99dd946775ba1bb845b2766
Mahmoud Alshbly <shbly1029@gmail.com>
2025-11-21 14:07:14 -0800

    refactor api services

--- lib/core/utlis/api_services.dart
+++ lib/core/utlis/api_services.dart
@@ -5,16 +5,20 @@ import 'package:dio/dio.dart';
 class ApiServices {
   final String baseUrl = '';
   static final ApiServices instance = ApiServices._internal();
-  ApiServices._internal();
+  factory ApiServices() => instance;
   late Dio dio;
-  void init() {
+  ApiServices._internal(){
     dio = Dio(
       BaseOptions(
         baseUrl: baseUrl,
         receiveDataWhenStatusError: true,
+        connectTimeout: const Duration(seconds: 20),
+        receiveTimeout: const Duration(seconds: 20),
       ),
     );
+
   }
+ 
   Future<dynamic> get({required String endPoin}) async {
     Response response = await dio.get(endPoin);
     return response.data;
