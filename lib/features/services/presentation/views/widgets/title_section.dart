6fbda6cd7be6feed5b2439b3797e2bc746fa2469
Mahmoud Alshbly <shbly1029@gmail.com>
2025-12-01 22:08:20 -0800

    refactor choose category view widgets

---
+++ lib/features/services/presentation/views/widgets/title_section.dart
@@ -0,0 +1,29 @@
+import 'package:booking_app/core/utils/constant.dart';
+import 'package:booking_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key,required this.title});
+  final String title ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+  TextSpan(
+    text: title, 
+    style: Styles.textStyle26.copyWith(color: kBlue), // الخط الأصلي
+    children: [
+      TextSpan(
+        text: '*',
+        style: Styles.textStyle26.copyWith(color: Colors.orange),
+      ),
+      TextSpan(
+        text: ':',
+        style: Styles.textStyle26.copyWith(color: kBlue),
+      ),
+    ],
+  ),
+  textDirection: TextDirection.rtl,
+);
+  }
+}
