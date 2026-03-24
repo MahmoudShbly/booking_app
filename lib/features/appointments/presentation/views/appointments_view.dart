3f0d16c50cbe30349d143608957304f17a1520f8
Mahmoud Alshbly <shbly1029@gmail.com>
2026-03-23 18:19:06 -0700

    add pull-to-refresh functionality in AppointmentsView

--- lib/features/appointments/presentation/views/appointments_view.dart
+++ lib/features/appointments/presentation/views/appointments_view.dart
@@ -1,15 +1,27 @@
 
+import 'package:booking_app/features/appointments/presentation/manager/fetch%20my%20bookings/fetch_my_bookings_cubit.dart';
 import 'package:booking_app/features/appointments/presentation/views/widgets/appointments_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppointmentsView extends StatelessWidget {
+class AppointmentsView extends StatefulWidget {
   const AppointmentsView({super.key});
 
   @override
+  State<AppointmentsView> createState() => _AppointmentsViewState();
+}
+ final refreshKey = GlobalKey<RefreshIndicatorState>();
+
+class _AppointmentsViewState extends State<AppointmentsView> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AppointmentsViewBody(),
-      
+    return RefreshIndicator(
+      key: refreshKey,
+      onRefresh: () => context.read<FetchMyBookingsCubit>().fetchMyBookings(),
+      child: Scaffold(
+        body: AppointmentsViewBody(),
+        
+      ),
     );
   }
 }
